5c831eaa7975e987ee341dc7e2c9d66ff05b6ca2
jaewoong <jwsuh25@kis.ac>
2023-03-21 13:02:32 +0900

    Deployment prep

--- webapp/app.py
+++ webapp/app.py
@@ -15,8 +15,20 @@ r = redis.Redis(
 
 app = Flask(__name__)
 
+class BC:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print(f'[{BC.OKCYAN}Event{BC.RESET}] {BC.BOLD}{BC.OKCYAN}GET{BC.RESET} request from {BC.BOLD}{BC.OKCYAN}{request.remote_addr}{BC.RESET}')
     if request.method == 'POST':
         user = request.form['twitter_handle']
         message = r.get(user)
@@ -29,4 +41,4 @@ def index():
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
